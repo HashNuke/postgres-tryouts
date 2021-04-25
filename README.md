f11bdbac3a3709d8799dbb9e2fec6828e08e4ab1
Akash Manohar <akash@akash.im>
2021-04-25 12:07:06 +0530

    Add readme and moma schema

---
+++ README.md
@@ -0,0 +1,8 @@
+# Postgres tryouts
+
+https://github.com/MuseumofModernArt/collection
+
+```
+git clone git@github.com:MuseumofModernArt/collection.git moma-collection
+```
+
---
+++ moma-schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE artworks(
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  artist_id INTEGER,
+  painting_date DATE,
+  classification TEXT,
+  department TEXT,
+  date_acquired TEXT,
+  width NUMERIC(8,2),
+  height NUMERIC(8,2),
+  website_url TEXT,
+  thumbnail_url TEXT
+);
+
+CREATE TABLE artists(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  bio TEXT,
+  nationality TEXT,
+  gender TEXT,
+  ulan TEXT,
+);
